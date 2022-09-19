3138cc95d099c2c77a3538410f5892fb17dbaeb2
Bogdan Popescu <bogdan.popescu2712@gmail.com>
2022-09-19 11:06:54 +0300

    Added functions implementations.

--- stats.c
+++ stats.c
@@ -26,28 +26,95 @@
 #define SIZE (40)
 
 void print_array(unsigned char* data, unsigned length){
+	unsigned char i;
+  	for (i=0; i< length; i++){
+  		printf("%d ", (unsigned char)data[i]);
+  	}
+  	printf("\n");
 }
 
 unsigned char find_mean(unsigned char* data, unsigned length){
+	unsigned short i, sum=0;
+  	for ( i=0; i<length; i++ ){
+  		sum+=data[i];
+  	}
+  	return (unsigned char)(sum/length);
 }
 
 unsigned char find_maximum(unsigned char* data, unsigned length){
+	unsigned char max, i;
+	max = data[0];
+	for (i=1; i<length; i++){
+		if (data[i]>max){
+			max = data[i];
+		}
+	}
+	return max;
 }
 
 unsigned char get_maxpos(unsigned char* data, unsigned length){
+	unsigned char i, maxpos, max;
+	maxpos = 0;
+	max = data[0];
+	for (i=1; i<length; i++){
+		if (data[i]>max){
+			max = data[i];
+			maxpos=i;
+		}
+	}
+	return maxpos;
 }
 
 unsigned char find_minimum(unsigned char* data, unsigned length){
+	unsigned char i, min;
+	min = data[0];
+	for (i=1; i<length; i++){
+		if (data[i]<min){
+			min = data[i];
+		}
+	}
+	return min;
 }
 
 unsigned char* sort_array(unsigned char* data, unsigned length){
+	unsigned char i, temp;
+	unsigned char maxpos, max;
+	for (i=0; i<length; i++){
+		max = find_maximum(&data[i],length-i);
+		maxpos = get_maxpos(&data[i],length-i);
+		temp=max;
+		data[maxpos+i]=data[i];
+		data[i]=temp;
+	}
 	return data;
 }
 
 unsigned char find_median(unsigned char* data, unsigned length){
+	unsigned char* sorted_data;
+	unsigned int median;
+
+	sorted_data = sort_array(data, length);
+
+	if (length%2 == 1){
+		median = sorted_data[length/2];
+		return sorted_data[length/2];
+	}
+	else{
+		median = (sorted_data[length/2-1]+sorted_data[length/2])/2;
+		return ((sorted_data[length/2-1]+sorted_data[length/2])/2);
+	}
 }
 
 void print_statistics(unsigned char* data, unsigned char length){
+	  printf("\nThese are printed from print_statistics: \n");
+
+	  printf("mean = %hu \n",find_mean(data, length));
+
+	  printf("max = %hu \n",find_maximum(data, length));
+
+	  printf("min = %hu \n",find_minimum(data, length));
+
+	  printf("median = %hu \n",find_median(data, length));
 }
 
 int main() {
